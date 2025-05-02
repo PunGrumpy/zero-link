dc653aa0eb0a0cf65cf4a11c5e9e725370fddfad
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-03 00:02:48 +0700

    feat(database): set up PostgreSQL database schema and migrations with Drizzle ORM integration

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+import { db } from '@/db'
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { nextCookies } from 'better-auth/next-js'
+import { env } from './env'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg'
+  }),
+  socialProviders: {
+    discord: {
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET
+    },
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    }
+  },
+  plugins: [nextCookies()]
+})
