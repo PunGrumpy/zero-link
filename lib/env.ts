df9668e378fdcad888b48647d531c8c7b40e2149
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-02 22:09:58 +0700

    feat(env): add environment configuration using @t3-oss/env-nextjs and update dependencies

---
+++ lib/env.ts
@@ -0,0 +1,9 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { vercel } from '@t3-oss/env-nextjs/presets-zod'
+
+export const env = createEnv({
+  extends: [vercel()],
+  server: {},
+  client: {},
+  runtimeEnv: {}
+})
