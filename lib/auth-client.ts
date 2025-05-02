a6b47ba588983e187305d2927dbd403c7620adad
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-03 00:32:06 +0700

    refactor(auth): simplify auth client configuration by removing baseURL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,3 @@
 import { createAuthClient } from 'better-auth/react'
-import { env } from './env'
 
-export const authClient = createAuthClient({
-  baseURL: env.BETTER_AUTH_URL
-})
+export const authClient = createAuthClient({})
