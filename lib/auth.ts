725c1f835e3e169405716a36397ef1ab62063b86
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-03 00:32:02 +0700

    feat(auth): integrate schema into betterAuth configuration for PostgreSQL provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import { db } from '@/db'
+import * as schema from '@/db/schema'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
@@ -6,7 +7,8 @@ import { env } from './env'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: 'pg'
+    provider: 'pg',
+    schema: schema
   }),
   socialProviders: {
     discord: {
