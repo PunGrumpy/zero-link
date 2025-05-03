765fa894dc18f601315437fa7a61a08d3d4ccbcf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-03 09:33:47 +0700

    feat(middleware): add authentication middleware to protect dashboard route

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import type { auth } from '@/lib/auth'
+import { betterFetch } from '@better-fetch/fetch'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export type Session = typeof auth.$Infer.Session
+
+export async function middleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    '/api/auth/get-session',
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get('cookie') || ''
+      }
+    }
+  )
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match protected routes
+     */
+    '/dashboard'
+  ]
+}
