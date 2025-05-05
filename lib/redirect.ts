0c05f6ee58129472f8e71243519e0a52c03e8e72
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-05 10:16:31 +0700

    feat(middleware): implement URL redirection based on slug and enhance route matching logic

---
+++ lib/redirect.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { db } from '@/db'
+import { link } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+
+type UrlFromSlugResponse = {
+  success: boolean
+  message: string
+  url: string
+}
+
+export const urlFromSlug = async (
+  slug: string
+): Promise<UrlFromSlugResponse> => {
+  if (!slug) {
+    return {
+      success: false,
+      message: 'Slug is required',
+      url: ''
+    }
+  }
+
+  const linkData = await db.select().from(link).where(eq(link.slug, slug))
+
+  if (!linkData || linkData.length === 0) {
+    return {
+      success: false,
+      message: 'Link not found',
+      url: ''
+    }
+  }
+
+  await db
+    .update(link)
+    .set({
+      clicks: linkData[0].clicks + 1
+    })
+    .where(eq(link.id, linkData[0].id))
+
+  return {
+    success: true,
+    message: 'Link found',
+    url: linkData[0].url
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,28 @@
 import type { auth } from '@/lib/auth'
 import { betterFetch } from '@better-fetch/fetch'
 import { type NextRequest, NextResponse } from 'next/server'
+import { urlFromSlug } from './lib/redirect'
 
 type Session = typeof auth.$Infer.Session
 
+const publicRoutes = ['/', '/login']
+const protectedRoutes = ['/dashboard', '/settings']
+
 export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname
+  const route = pathname.split('/').pop()
+
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next()
+  }
+
+  if (route && !protectedRoutes.includes(route)) {
+    const data = await urlFromSlug(route)
+    if (data.success) {
+      return NextResponse.redirect(new URL(data.url, request.url))
+    }
+  }
+
   const { data: session } = await betterFetch<Session>(
     '/api/auth/get-session',
     {
@@ -25,8 +43,12 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match protected routes
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/dashboard'
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
   ]
 }
