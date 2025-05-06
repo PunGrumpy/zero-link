ca10e389309099f57fcdf0881c6ef337461dedd2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-06 22:24:01 +0700

    feat(auth): add base URL configuration and define subscription plans for better user management

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,12 +6,28 @@ import { nextCookies } from 'better-auth/next-js'
 import { admin, twoFactor } from 'better-auth/plugins'
 import { env } from './env'
 
+const baseUrl = env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : 'http://localhost:3000'
+
+export const Plan = {
+  Starter: {
+    productId: 'aa6e0754-83c0-496a-adb4-63c3a28a065a',
+    slug: 'starter'
+  },
+  Pro: {
+    productId: '74b7a038-8254-47b5-8ef3-7d83380dc75f',
+    slug: 'pro'
+  }
+}
+
 export const auth = betterAuth({
   appName: 'Zero Link',
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema: schema
   }),
+  trustedOrigins: [baseUrl],
   user: {
     additionalFields: {
       limitLinks: {
