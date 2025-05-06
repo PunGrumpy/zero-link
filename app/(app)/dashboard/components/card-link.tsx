fd46ee7b45699f695af4844536f884ec0a73135e
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-06 15:00:13 +0700

    refactor(card-link): remove sorting logic and update QR code and copy link components to use dynamic base URL

--- app/(app)/dashboard/components/card-link.tsx
+++ app/(app)/dashboard/components/card-link.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { buttonVariants } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import {
@@ -8,6 +6,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
+import { env } from '@/lib/env'
 import { cn, formatDate, getTagBackgroundColor } from '@/lib/utils'
 import { Copy, QrCode } from 'lucide-react'
 import Link from 'next/link'
@@ -15,38 +14,19 @@ import type { LinkWithTag } from '../../actions/link'
 import { CopyLink } from './copy-link'
 import { QRCode } from './qr-code'
 import { ShowClick } from './show-click'
-import { useSortLink } from './sort-link'
 
 type CardLinkProps = {
   filteredLink: LinkWithTag[]
 }
 
 export const CardLink = ({ filteredLink }: CardLinkProps) => {
-  const { sort } = useSortLink()
-
-  const sortedLinks = filteredLink.sort((a, b) => {
-    if (sort === 'newest') {
-      return b.createdAt.getTime() - a.createdAt.getTime()
-    }
-
-    if (sort === 'oldest') {
-      return a.createdAt.getTime() - b.createdAt.getTime()
-    }
-
-    if (sort === 'most-clicks') {
-      return b.clicks - a.clicks
-    }
-
-    if (sort === 'least-clicks') {
-      return a.clicks - b.clicks
-    }
-
-    return 0
-  })
+  const baseUrl = env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : 'http://localhost:3000'
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
-      {sortedLinks.map(link => (
+      {filteredLink.map(link => (
         <div
           key={link.id}
           className={cn(
@@ -75,7 +55,7 @@ export const CardLink = ({ filteredLink }: CardLinkProps) => {
                     <Copy className="h-4 w-4" />
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
-                    <CopyLink slug={link.slug} />
+                    <CopyLink baseUrl={baseUrl} slug={link.slug} />
                     <DialogTrigger asChild>
                       <DropdownMenuItem>
                         <QrCode className="h-4 w-4" /> Copy QR Code
@@ -83,7 +63,7 @@ export const CardLink = ({ filteredLink }: CardLinkProps) => {
                     </DialogTrigger>
                   </DropdownMenuContent>
                 </DropdownMenu>
-                <QRCode linkInfo={link} />
+                <QRCode baseUrl={baseUrl} linkInfo={link} />
               </Dialog>
             </div>
           </section>
